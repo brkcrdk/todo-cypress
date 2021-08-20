c84be991fc73410dfa3aeb322fd5be328c8e8244
Burak <burak@90pixel.com>
2021-08-20 12:09:11 +0300

    Add globalStyle.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import GlobalStyle from 'styles/globalStyles';
+import 'styles/libraryStyles.css';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
