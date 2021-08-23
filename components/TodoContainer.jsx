62fb09b56d0c2b171fe5d3736eef30f3042a70fe
Burak <burak@90pixel.com>
2021-08-23 17:20:19 +0300

    Fix semantic ui issue.

--- components/TodoContainer.jsx
+++ components/TodoContainer.jsx
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
-import { List } from 'semantic-ui-react';
 
 function TodoContainer({ children, ...props }) {
-  return <StyledList {...props}>{children}</StyledList>;
+  return <List {...props}>{children}</List>;
 }
 export default TodoContainer;
 
-const StyledList = styled(List)`
+const List = styled.ul`
   width: 100% !important;
 `;
