48747bd80e6c6fe2d8beec1473eb8e0dc07c4dc8
Burak <burak@90pixel.com>
2021-08-23 17:18:50 +0300

    Add todo container component.

--- components/TodoContainer.jsx
+++ components/TodoContainer.jsx
@@ -1,4 +1,11 @@
-function TodoContainer() {
-  return <div>TodoContainer is here</div>;
+import styled from 'styled-components';
+import { List } from 'semantic-ui-react';
+
+function TodoContainer({ children, ...props }) {
+  return <StyledList {...props}>{children}</StyledList>;
 }
 export default TodoContainer;
+
+const StyledList = styled(List)`
+  width: 100% !important;
+`;
