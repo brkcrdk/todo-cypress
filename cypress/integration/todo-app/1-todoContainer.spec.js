3eb4f6f51867fb1e29b08115fff5d76475b3ee05
Burak <burak@90pixel.com>
2021-08-25 16:34:21 +0300

    Update todoContainer tests.

--- cypress/integration/todo-app/1-todoContainer.spec.js
+++ cypress/integration/todo-app/1-todoContainer.spec.js
@@ -1,8 +1,22 @@
 describe('Todo uygulaması başarılı bir şekilde', () => {
-  it('Başarılı bir şekilde açılıyor ve boş liste ile geliyor.', () => {
-    cy.visit('/');
+  beforeEach(() => cy.visit('/'));
+
+  it('Başarılı bir şekilde açılıp, boş liste ile gelmeli.', () => {
     cy.get('[data-cy=todo-header]').contains('Yapılacak Listesi');
     cy.get('[data-cy=empty-message]').should('exist');
+    cy.get('[data-cy=add-todo-section]').should('exist');
     cy.get('[data-cy=todo-container]').should('not.exist');
+  });
+
+  it('Eğer listede eleman varsa boş mesajı gelmemeli ve controller görüntülenmeli', () => {
+    cy.get('[data-cy=empty-message]').should('exist');
+    cy.get('[data-cy=todo-input]').type(`Deneme`);
+    cy.get('[data-cy=todo-add-btn]').click();
+    cy.get('[data-cy=empty-message]').should('not.exist');
+    cy.get('[data-cy=todo-container]').should('exist');
+    cy.reload();
+    cy.get('[data-cy=empty-message]').should('not.exist');
+    cy.get('[data-cy=todo-container]').should('exist');
+    cy.get('[data-cy=todo-container]').children().should('have.length', 1);
   });
 });
