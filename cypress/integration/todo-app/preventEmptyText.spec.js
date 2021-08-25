68827c7fcc5597e6275d8687eb9b8c5af12e216c
Burak <burak@90pixel.com>
2021-08-25 15:02:35 +0300

    Seperate test.

---
+++ cypress/integration/todo-app/preventEmptyText.spec.js
@@ -0,0 +1,15 @@
+describe('Eğer input boşsa todo kayıt edilememeli', () => {
+  beforeEach(() => cy.visit('/'));
+  it('Eğer input boşsa button disabled olmalı', () => {
+    cy.get('[data-cy=todo-add-btn]').should('have.class', 'disabled');
+    cy.get('[data-cy=todo-input]').type('deneme yazısı');
+    cy.get('[data-cy=todo-add-btn]').should('not.have.class', 'disabled');
+    cy.get('[data-cy=todo-input]').clear();
+    cy.get('[data-cy=todo-add-btn]').should('have.class', 'disabled');
+  });
+  it('Eğer entera basıldığında input boşsa todo eklenmemeli', () => {
+    const expectedText = 'enter deneme yazısı';
+    cy.get('[data-cy=todo-input]').type(`${expectedText} {enter}`);
+    cy.get('[data-cy=todo-element]').should('have.text', expectedText);
+  });
+});
