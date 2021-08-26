590be698d4def3f210195c492ded2b5c9fdfe947
Burak <burak@90pixel.com>
2021-08-26 15:29:19 +0300

    Add delete todo method.

--- store/todoStore.js
+++ store/todoStore.js
@@ -3,6 +3,12 @@ const todoStore = (set, get) => {
     todos: [],
     addTodo: (todo) => set({ todos: get().todos.concat(todo) }),
     clearAll: () => set({ todos: [] }),
+    deleteTodo: (todoIndex) => {
+      const filteredTodo = get().todos.filter(
+        (todo, index) => index !== todoIndex
+      );
+      set({ todos: filteredTodo });
+    },
   };
 };
 
