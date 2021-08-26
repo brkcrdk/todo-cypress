9aee8b22e96edd2410deaca78cfb249c23b61f08
Burak <burak@90pixel.com>
2021-08-26 11:14:53 +0300

    Add todo section tests

---
+++ cypress/integration/todo-app/2-addTodoSection.spec.js
@@ -0,0 +1,25 @@
+describe('Eğer input boşsa todo kayıt edilememeli', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('Eğer input boşsa button disabled olmalı', () => {
+    cy.get('[data-cy=todo-add-btn]').should('have.class', 'disabled');
+    cy.get('[data-cy=todo-input]').type('deneme yazısı');
+    cy.get('[data-cy=todo-add-btn]').should('not.have.class', 'disabled');
+    cy.get('[data-cy=todo-input]').clear();
+    cy.get('[data-cy=todo-add-btn]').should('have.class', 'disabled');
+  });
+
+  it('Eğer entera basıldığında input boşsa todo eklenmemeli', () => {
+    const expectedText = 'enter deneme yazısı';
+    cy.get('[data-cy=todo-input]').type(`${expectedText}{enter}`);
+    cy.get('[data-cy=todo-element]').should('have.text', expectedText);
+    cy.get('[data-cy=todo-input]').clear().type('{enter}');
+    cy.get('[data-cy=todo-container]').children().should('have.length', 1);
+  });
+
+  it('Bir todo kaydedildiğinde input temizlenmiş olmalı', () => {
+    cy.get('[data-cy=todo-input]').type('Deneme');
+    cy.get('[data-cy=todo-add-btn]').click();
+    cy.get('[data-cy=todo-input]').children('input').should('have.value', '');
+  });
+});
