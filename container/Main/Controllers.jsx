a6adfe2519e002cd9aac6a760320faa3aaed0b83
Burak <burak@90pixel.com>
2021-08-27 10:00:59 +0300

    Pass filter method to controllers.

--- container/Main/Controllers.jsx
+++ container/Main/Controllers.jsx
@@ -4,7 +4,7 @@ import { Button, Radio } from 'semantic-ui-react';
 import useStoreMacro from '@90pixel/zustand.macro';
 
 function Controllers() {
-  const { clearAll } = useStoreMacro();
+  const { clearAll, filterTodo } = useStoreMacro();
   const [type, setType] = useState('all');
 
   return (
@@ -12,19 +12,28 @@ function Controllers() {
       <Radio
         label="Hepsi"
         checked={type === 'all'}
-        onChange={() => setType('all')}
+        onChange={() => {
+          setType('all');
+          filterTodo('all');
+        }}
         data-cy="controller-all"
       />
       <Radio
         label="Yapılacaklar"
         checked={type === 'todos'}
-        onChange={() => setType('todos')}
+        onChange={() => {
+          setType('todos');
+          filterTodo('todos');
+        }}
         data-cy="controller-todos"
       />
       <Radio
         label="Yapılmışlar"
         checked={type === 'completed'}
-        onChange={() => setType('completed')}
+        onChange={() => {
+          setType('completed');
+          filterTodo('completed');
+        }}
         data-cy="controller-completed"
       />
       <Button onClick={clearAll} data-cy="controller-clear">
